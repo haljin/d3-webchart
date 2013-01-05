384f89ea63fc64d8677b7dcc9ae75766d4d77ee8
Pawel Antemijczuk <paul.shinobi@gmail.com>
2013-01-05 18:02:08 +0100

    I have no fucking idea

---
+++ Sensible/libs/calendar.js
@@ -0,0 +1,28 @@
+﻿var Calendar;
+
+Calendar = (function () {
+
+    function Calendar() {
+        this.startDate = (new Date(Date.now())).setMonth((new Date(Date.now())).getMonth() - 1);
+        this.endDate = Date.now();
+        this.svg = null;
+
+        this.drawCalendar();
+    };
+
+    Calendar.prototype.drawCalendar = function () {
+        
+
+    };
+
+    return Calendar;
+})();
+
+
+
+
+$(function () {
+    var cal;
+    cal = new Calendar();
+});
+
