63d52227369f3ee9ba5ce0a31b34da272bde7b0c
Pawel Antemijczuk <paul.shinobi@gmail.com>
2013-05-30 11:46:38 +0200

    processing for map

--- Sensible/js/DataProcessor.js
+++ Sensible/js/DataProcessor.js
@@ -5,5 +5,62 @@ DataProcessor = (function () {
 
     };
 
+
+    DataProcessor.prototype.generate_loc_data = function (btData, smsData, callData, locData) {
+        var results = [];
+
+        callData.forEach(function (d) {
+            var diff = locData[0].timestamp;
+            var closest = null;
+            
+            for (var i = 0; i < locData.length; i++) {
+                if (Math.abs(locData[i].timestamp - d.timestamp) <= diff) {
+                    diff = Math.abs(locData[i].timestamp - d.timestamp);
+                    closest = x;
+                }
+                else
+                    break;
+            };
+
+            results.push({ lon: closest.location.longtitude, lat: closest.location.latitude, type: "call" });
+        });
+
+        smsData.forEach(function (d) {
+            var diff = locData[0].timestamp;
+            var closest = null;
+
+            for (var i = 0; i < locData.length; i++) {
+                if (Math.abs(locData[i].timestamp - d.timestamp) <= diff) {
+                    diff = Math.abs(locData[i].timestamp - d.timestamp);
+                    closest = x;
+                }
+                else
+                    break;
+            };
+
+            results.push({ lon: closest.location.longtitude, lat: closest.location.latitude, type: "sms" });
+        });
+
+        btData.forEach(function (d) {
+            var diff = locData[0].timestamp;
+            var closest = null;
+
+            for (var i = 0; i < locData.length; i++) {
+                if (Math.abs(locData[i].timestamp - d.timestamp) <= diff) {
+                    diff = Math.abs(locData[i].timestamp - d.timestamp);
+                    closest = x;
+                }
+                else
+                    break;
+            };
+
+            results.push({ lon: closest.location.longtitude, lat: closest.location.latitude, type: "bt" });
+        });
+
+
+
+
+    };
+
     return DataProcessor;
 })();
