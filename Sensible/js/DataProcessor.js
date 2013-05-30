6469afeb2745643b9e9bf2cd28a88a857658a439
Marta Magiera <corny11@gmail.com>
2013-05-30 14:20:04 +0200

    timeline

--- Sensible/js/DataProcessor.js
+++ Sensible/js/DataProcessor.js
@@ -6,7 +6,22 @@ DataProcessor = (function () {
 
     };
 
-    DataProcessor.prototype.gen_timeline_date = function (btData, smsData, callData) {
+
+    DataProcessor.prototype.gen_timeline_data = function (data) {
+
+        var result = [[], [], []];
+       
+
+        data.data.forEach(function (d, i) {
+            result[0].push({ date: new Date(d.date), x: i, y: d.btcount, y0: 0 });
+            result[1].push({ date: new Date(d.date), x: i, y: d.callcount, y0: d.btcount });
+            result[2].push({ date: new Date(d.date), x: i, y: d.smscount, y0: d.callcount + d.btcount });
+        });
+
+        return result;
+    };
+
+    DataProcessor.prototype.parse_collective_data = function (btData, smsData, callData) {
         var result = [];
 
         btData.forEach(function (d) {
@@ -48,7 +63,7 @@ DataProcessor = (function () {
         result.sort(function (a, b) {
             return a.date - b.date;
         });
-        return result;
+        return window.open("data:text/json;charset=utf-8," + escape(JSON.stringify({ data: result }, null, 2)));
     };
 
     return DataProcessor;
